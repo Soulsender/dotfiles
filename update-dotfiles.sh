3ecd193703d34121e7b52542409946f89bdd2d10
Soulsender <soul@soulsender.me>
2024-11-17 12:04:11 -0800

    update xps15 dots

--- update-dotfiles.sh
+++ update-dotfiles.sh
@@ -4,7 +4,7 @@
 # dotfiles are different between hosts
 mkdir -p $HOSTNAME
 
-cp -f $HOME/.zshrc ./$HOSTNAME/
+cp -f $HOME/.zshrc ./$HOSTNAME/zshrc
 cp -rf $HOME/.config/ags ./$HOSTNAME/
 cp -rf $HOME/.config/hypr ./$HOSTNAME/
 cp -rf $HOME/.config/kitty ./$HOSTNAME/
