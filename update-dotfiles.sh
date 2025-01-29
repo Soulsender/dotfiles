6a9d22bc790b342f09e7835341b4f503f32b4eab
Soulsender <soul@soulsender.me>
2025-01-29 09:26:45 -0800

    added list of packages

--- update-dotfiles.sh
+++ update-dotfiles.sh
@@ -10,3 +10,7 @@ cp -rf $HOME/.config/ags ./$HOSTNAME/
 cp -rf $HOME/.config/hypr ./$HOSTNAME/
 cp -rf $HOME/.config/kitty ./$HOSTNAME/
 cp -rf $HOME/Documents/notes/.obsidian ./$HOSTNAME/obsidian
+
+# get a list of all installed pacman and AUR packages
+pacman -Qqe > ./$HOSTNAME/packages-pacman.txt
+pacman -Qqm > ./$HOSTNAME/packages-AUR.txt
