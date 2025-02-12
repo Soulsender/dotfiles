5fe2252290ea8ee52aa7577aa11c77651b0198f6
Soulsender <soul@soulsender.me>
2025-02-12 11:08:29 -0800

    update fuzzel

--- xps15/scripts/update-certificates.sh
+++ xps15/scripts/update-certificates.sh
@@ -1,10 +1,9 @@
+
 #!/bin/bash
 
-# this script automatically copies the tailsale SSL certificates to my laptop
+# update certificate store for tailscale
 
 ORIGINAL_USER=$USER
-
-sudo mkdir -p /usr/share/ca-certificates/tailscale
 
 sudo scp -i /home/$ORIGINAL_USER/.ssh/id_rsa root@100.115.198.67:/root/dockerhost.pigeon-gecko.ts.net.crt /usr/share/ca-certificates/tailscale/
 sudo scp -i /home/$ORIGINAL_USER/.ssh/id_rsa root@100.67.182.42:/root/proxmox.pigeon-gecko.ts.net.crt /usr/share/ca-certificates/tailscale/
