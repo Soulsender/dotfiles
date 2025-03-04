3e357f5abab6de7bde0882a65f8b88b47b9d21ea
Soulsender <soul@soulsender.me>
2025-03-03 16:45:38 -0800

    update

--- update-dotfiles.sh
+++ update-dotfiles.sh
@@ -9,6 +9,7 @@ cp -f $HOME/.zshrc ./$HOSTNAME/zshrc
 cp -f $HOME/.config/code-flags.conf ./$HOSTNAME/code-flags.conf
 cp -rf $HOME/.config/ags ./$HOSTNAME/
 cp -rf $HOME/.config/hypr ./$HOSTNAME/
+cp -rf $HOME/.config/fuzzel ./$HOSTNAME/
 cp -rf $HOME/.config/kitty ./$HOSTNAME/
 cp -rf $HOME/Documents/notes/.obsidian ./$HOSTNAME/obsidian
 
